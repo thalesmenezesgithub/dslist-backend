a9605483f6a984e85315cccdaff3c2ae296273ce
Thales R. Menezes <99663601+thalesmenezesgithub@users.noreply.github.com>
2023-06-14 10:46:53 -0300

    Implementação
    GameList
    GameMinDTO
    GameMinProjection
    GameRepository
    GameService

--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.service;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entity.Game;
+import com.devsuperior.dslist.projection.GameMinProjection;
 import com.devsuperior.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -39,5 +40,20 @@ public class GameService
         Game result = gameRepository.findById(id).get();
 
         return new GameDTO(result);
+    }
+
+    @Transactional
+    public List<GameMinDTO> findByList(Long listId)
+    {
+        /*
+         *  Realiza uma pesquisa por ListId
+         *  retorna projection
+         */
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+
+        //Transforma uma Lista de Games em um Objeto DTO
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+
+        return dto;
     }
 }
